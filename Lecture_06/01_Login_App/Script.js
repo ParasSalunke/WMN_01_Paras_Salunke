416186889799092183bf9bcb5a5fef4c4a70792f
ParasSalunke-24 <salunkeparas24@gmail.com>
2024-07-25 10:15:13 +0530

    25-07-2024 10:15 PM

---
+++ Lecture_06/01_Login_App/Script.js
@@ -0,0 +1,7 @@
+function submitUserInformation(){
+    var userName = document.getElementById('username_input').value;
+    var password = document.getElementById('password_input').value;
+
+    alert=("Username:" + userName.value + "Password:" + password.value);
+    
+}
