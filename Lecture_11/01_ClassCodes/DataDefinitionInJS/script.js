04ed1f075891adc619cdcb91c0d095c274329811
ParasSalunke-24 <salunkeparas24@gmail.com>
2024-08-06 22:25:44 +0530

    06-08-2024 10:25 PM - Demonstration of Data Definition Statement's in JS

---
+++ Lecture_11/01_ClassCodes/DataDefinitionInJS/script.js
@@ -0,0 +1,12 @@
+
+//	Data Definition Statement
+
+let num = "how are you";
+var inum;
+
+var title = document.querySelector("h1");
+var heading2 = document.querySelector("h2");
+
+title.textContent = "Hello Marshalling Void Students"
+
+heading2.textContent = num
