1f3a97cbee69bc5e1cd3ae3bcdd86cb548becec9
ParasSalunke-24 <salunkeparas24@gmail.com>
2024-08-28 23:17:02 +0530

    11:16 PM - 28/08/2024 - Implementation of Array Basics

---
+++ Lecture_18/01_ClassCodes/01_Accessing_Array_Elements/script.js
@@ -0,0 +1,15 @@
+//	Declaration of Array
+
+// let arr = new Array();
+// let arr = []
+
+let colors = ["Red", "Green", "Blue"];
+
+//	Array element access
+
+alert(typeof [])
+
+alert(colors[0])
+alert(colors[1])
+alert(colors[2])
+alert(colors[3])
---
+++ Lecture_18/01_ClassCodes/02_Accessing_using_loop/script.js
@@ -0,0 +1,13 @@
+//	Declaration of Array
+
+// let arr = new Array();
+// let arr = []
+
+let colors = ["Red", "Green", "Blue", "Cyan", "Magenta", "Yellow", "White", "Black"];
+
+//	Array element access
+
+//	Access Array elements using Loop
+for (let le = 0; le < colors.length; ++le) {
+	alert(colors[le]);
+}
